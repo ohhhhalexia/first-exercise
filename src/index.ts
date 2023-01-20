9f5436875be1be40bdd3f9cddaced55d42c9aebd
alexia rodriguez <alexia.rodriguez11211@gmail.com>
2023-01-20 09:50:23 -0600

    before online learning

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 // PART ONE
 
 // function mergeArray (array: Array<number>): Array<number> {
-    return mergedArray;
+//    return mergedArray;
+// }
+function merge(arr1: Array<number>, arr2: Array<number>): Array<number> {
+  //    const arr1 = array1.push()
+  //    const arr2 = array2
+  //    return
 }
 
 const array1: Array<number> = [4, 5, 23, 18, 9, -5];
@@ -10,9 +15,6 @@ const array2: Array<number> = [18, 74, 88, 3, 7, 44];
 const mergedArray: Array<number> = merge(array1, array2);
 console.log(mergedArray);
 
-
 // function merge(arr1: Array<number>, arr2: Array<number>): Array<number> {
-    
-}
 
-
+// }
